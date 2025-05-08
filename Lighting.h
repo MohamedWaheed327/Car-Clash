9d76d5d6e6dde461bfd5957cdecac517fbc30efa
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2025-05-09 02:36:13 +0300

    last

--- Lighting.h
+++ Lighting.h
@@ -4,6 +4,6 @@
 #include <gl/glut.h>
 
 void setupLighting() {
-    GLfloat light_pos[] = {0, 6.0, -car_y+15, 1.0};
+    GLfloat light_pos[] = {0, 6.0, -car_y + 15, 1.0};
     glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
 }
