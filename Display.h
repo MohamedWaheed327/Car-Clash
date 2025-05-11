161492cd9df997ee3652fabd9ce98c15e7b80f4a
MohamedWaheed327 <mohamedwaheedsa327@gmail.com>
2025-05-12 01:07:54 +0300

    removed two parameters

--- Display.h
+++ Display.h
@@ -19,8 +19,6 @@ GLuint loadTexture(const char *filename) {
     glBindTexture(GL_TEXTURE_2D, textureID);
 
     // Set wrapping/filtering
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
